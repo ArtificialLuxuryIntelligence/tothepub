33d0969e3448ca253c735182fcd36a35b8171b9d
Ali <adgk.dev@gmail.com>
2020-09-28 20:38:53 +0100

    fix webpack prod file fix

--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -1,22 +1,22 @@
-const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
-const Dotenv = require("dotenv-webpack");
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
-const buildPath = path.resolve(__dirname, "dist");
+const buildPath = path.resolve(__dirname, 'dist');
 
 module.exports = {
-  devtool: "source-map",
+  devtool: 'source-map',
 
   entry: {
-    index: "./src/index.js",
+    index: './src/index.js',
   },
 
   output: {
-    filename: "[name].[hash:20].js",
+    filename: '[name].[hash:20].js',
     path: buildPath,
   },
 
@@ -25,38 +25,44 @@ module.exports = {
     new MiniCssExtractPlugin(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      inject: "body",
-      chunks: ["index"],
-      filename: "index.html",
+      template: './src/admin/admin.html',
+      inject: true,
+      chunks: ['admin'],
+      filename: './admin/index.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      inject: true,
+      chunks: ['main'],
+      filename: 'index.html',
     }),
   ],
   module: {
     rules: [
       {
         test: /\.html$/i,
-        loader: "html-loader",
+        loader: 'html-loader',
       },
       {
         test: /\.js$/i,
         exclude: /node_modules/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         options: {
-          presets: ["@babel/preset-env"],
+          presets: ['@babel/preset-env'],
         },
       },
       {
         test: /\.(sc|c)ss$/i,
         use: [
           MiniCssExtractPlugin.loader,
-          "css-loader",
+          'css-loader',
           {
-            loader: "postcss-loader",
+            loader: 'postcss-loader',
             options: {
               postcssOptions: {
                 plugins: [
                   [
-                    "autoprefixer",
+                    'autoprefixer',
                     {
                       // Options
                     },
@@ -65,14 +71,14 @@ module.exports = {
               },
             },
           },
-          "sass-loader",
+          'sass-loader',
         ],
       },
       {
         test: /\.(png|jpe?g|gif|svg)$/i,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
           },
         ],
       },
