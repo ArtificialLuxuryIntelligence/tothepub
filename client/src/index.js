a53fb92bfae967c7ed325989e52b194090a50917
Ali <adgk.dev@gmail.com>
2020-10-12 16:05:24 +0100

    page loader and error messages

--- client/src/index.js
+++ client/src/index.js
@@ -5,19 +5,30 @@ import geolocate from './scripts/geolocate';
 import { getAllTags, getLocalTags } from './scripts/getTags';
 import findNearest from './scripts/findNearest';
 import drawMap from './scripts/drawMap';
+import { showError } from './scripts/helpers';
 
 // categories to include in drop down (of allTags [from database])
 // reason: some tags are good to have on map but not in search
 // DEPRECATED dropDownTags  -- now dropdown only shows available local tags
 const dropDownTags = ['operator', 'food', 'amenity', 'real-ale'];
-
 const dropDown = document.getElementById('tag-dropdown');
 const takeMeButton = document.getElementById('take-me');
-let allTags;
+let allTags, localTags;
 let longitude, latitude;
 
+// Init
 (async () => {
-  const { coords } = await geolocate();
+  let coords;
+  try {
+    let geo = await geolocate();
+    coords = geo.coords;
+  } catch (error) {
+    //geo locate is disabled
+    showError('Please enable geolocation and refresh page');
+    removeLoader(300);
+    return;
+  }
+
   if (process.env.MODE === 'dev') {
     longitude = -0.13703469999999998;
     latitude = 51.520621633333336;
@@ -26,18 +37,23 @@ let longitude, latitude;
     longitude = coords.longitude;
   }
 
-  allTags = await getAllTags();
-  let localTags = await getLocalTags(longitude, latitude);
-  // console.log(localTags);
-  populateDropDown(allTags, localTags, dropDownTags);
+  try {
+    allTags = await getAllTags();
+    localTags = await getLocalTags(longitude, latitude);
+    populateDropDown(allTags, localTags, dropDownTags);
+  } catch (error) {
+    showError('Server errror - please refresh page or try again later');
+    removeLoader(300);
+  }
+  takeMeButton.addEventListener('click', () => takeMeToThePub(25)); //note: server limit is currently 25 results
+  removeLoader(200);
 })();
 
-function toggleLoading() {
-  takeMeButton.classList.add('animate');
-}
+// -----------------
 
 async function takeMeToThePub(maxResults) {
-  toggleLoading();
+  toggleMapLoading();
+
   try {
     //Get current position
     const start = [longitude, latitude];
@@ -48,25 +64,33 @@ async function takeMeToThePub(maxResults) {
     drawMap(start, nearest, allTags, tag);
     //add button to choose next pub in list of nearest
   } catch (error) {
-    // TO DO //
-    // Handle error (i.e. user denies geolocation)
-    console.error(error);
+    showError('Server errror - please refresh page or try again later');
   }
 }
 
-// Button event listener
-takeMeButton.addEventListener('click', () => takeMeToThePub(25)); //note: server limit is currently 25 results
+function toggleMapLoading() {
+  takeMeButton.classList.add('animate');
+}
+
+function removeLoader(delay) {
+  setTimeout(() => {
+    const loader = document.getElementById('page-loader');
+    loader.classList.add('unload');
+    setTimeout(() => {
+      loader.parentNode.removeChild(loader);
+    }, 2000);
+  }, delay);
+}
 
 function populateDropDown(allTags, localTags, dropDownTags) {
   //call api for list of tags
 
-  // console.log(allTags);
   //dropDownTags is hardcoded array of accepted categories for dropdown
   // let ddallTags = allTags.filter((o) => dropDownTags.includes(o.category));
   let ddallTags = allTags;
   ddallTags.forEach((cat) => {
     if (!cat.tags.some((tag) => localTags.includes(tag.tag))) {
-      return; //no local tags in this category (assuming there will be at least one over the count limit)
+      return; //no local tags in this category
     }
     let op = document.createElement('option');
     op.disabled = 'disabled';
@@ -84,6 +108,6 @@ function populateDropDown(allTags, localTags, dropDownTags) {
   });
 }
 
-function capitalise(word) {
-  return word.charAt(0).toUpperCase() + word.slice(1);
-}
+// function capitalise(word) {
+//   return word.charAt(0).toUpperCase() + word.slice(1);
+// }
